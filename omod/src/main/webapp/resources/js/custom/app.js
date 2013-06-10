1dda1136546c3e0509aef1aaf3c27fd22bfde8e9
Shashank Tomar <shashank004@gmail.com>
2013-06-10 18:31:23 +0530

    Display forms on select on the list of forms

--- omod/src/main/webapp/resources/js/custom/app.js
+++ omod/src/main/webapp/resources/js/custom/app.js
@@ -13,9 +13,9 @@ html5formsModule.factory('FormService', function () {
 
     service.forms = [
         {id: '1', name: 'Patient Registration Form', description: 'Form for registering patients', selected: false, tags: [1, 2]},
-        {id: '2', name: 'Encounter Form', description: 'Form for capturing patient encounters', selected: false, tags: [2, 5]},
-        {id: '3', name: 'Adult Initial HIV Encounter Form', description: 'Form for capturing initial HIV encounter for an adult', selected: false, tags: [2,4,6,8]},
-        {id: '4', name: 'PMTCT Ante-Natal Care Form', description: '', selected: false, tags: [1,5,2,7]},
+        {id: '2', name: 'PMTCT Ante-Natal Care Form', description: '', selected: false, tags: [1,5,2,7]},
+        {id: '3', name: 'Encounter Form', description: 'Form for capturing patient encounters', selected: false, tags: [2, 5]},
+        {id: '4', name: 'Adult Initial HIV Encounter Form', description: 'Form for capturing initial HIV encounter for an adult', selected: false, tags: [2,4,6,8]},
         {id: '5', name: 'Pediatric Summary Form', description: 'Form for capturing pediatric summary', selected: false, tags: [9]},
         {id: '6', name: 'Outreach Adult Locator Form', description: '', selected: false, tags: [3,4,8]}
     ];
--- omod/src/main/webapp/resources/js/custom/controller.js
+++ omod/src/main/webapp/resources/js/custom/controller.js
@@ -2,16 +2,16 @@ function FormCtrl($scope, FormService, TagService) {
     $scope.tags = TagService.tags;
     $scope.forms = FormService.forms;
 
-//    $scope.selectedFormId = $scope.forms[0].id;
+    $scope.selectedFormId = $scope.forms[0].id;
 
-//    $scope.getPreviewFormPath = function () {
-//        return '../../moduleResources/html5forms/html5forms/form-' + $scope.selectedFormId + '.html';
-//    };
+    $scope.getPreviewFormPath = function () {
+        return '../../moduleResources/html5forms/html5forms/form-' + $scope.selectedFormId + '.html';
+    };
 
-//    $scope.selectForm = function (id) {
-//        $scope.selectedFormId = id;
-//    };
-//
+    $scope.select = function (id) {
+        $scope.selectedFormId = id;
+    };
+
     $scope.getTagStyle = function (tagId) {
         console.log(TagService.tagColor(tagId));
         return  {'background-color': TagService.tagColor(tagId)};

11e8ed622f918643699b948fa197d9be56d7c000
Shashank Tomar <shashank004@gmail.com>
2013-06-17 11:00:49 +0530

    Fixed controller test

--- omod/src/main/webapp/resources/js/custom/controller.js
+++ omod/src/main/webapp/resources/js/custom/controller.js
@@ -1,11 +1,6 @@
 function FormCtrl($scope, FormService, XFormService, TagService) {
     $scope.tags = TagService.tags();
-    var forms = { list : [{"id":"1","name":"Patient Registration Form","description":"Form for registering patients","selected":false,"tags":[1,2]},
-        {"id":"2","name":"PMTCT Ante-Natal Care Form","description":"","selected":false,"tags":[1,5,2,7]},
-        {"id":"3","name":"Encounter Form","description":"Form for capturing patient encounters","selected":false,"tags":[2,5]},
-        {"id":"4","name":"Adult Initial HIV Encounter Form","description":"Form for capturing initial HIV encounter for an adult","selected":false,"tags":[2,4,6,8]},
-        {"id":"5","name":"Pediatric Summary Form","description":"Form for capturing pediatric summary","selected":false,"tags":[9]},
-        {"id":"6","name":"Outreach Adult Locator Form","description":"","selected":false,"tags":[3,4,8]}]};
+    var forms = FormService.forms();
     var xForms = XFormService.xForms();
     var selectedXForms = [];
     var selectedFormId;

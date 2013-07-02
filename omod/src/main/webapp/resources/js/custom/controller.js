71207fb5697f14812a1ffabc5198e4c64c6a62e1
Ranjan Sakalley <rsakalle@thoughtworks.com>
2013-07-02 18:23:22 +0530

    minor refactoring - changing names etc.

--- omod/src/main/webapp/resources/js/custom/controller.js
+++ omod/src/main/webapp/resources/js/custom/controller.js
@@ -63,13 +63,12 @@ function FormCtrl($scope, FormService, FormsService, XFormService, TagService, _
         return !_.isEmpty($scope.html5forms);
     };
 
-    $scope.getPreviewFormPath = function () {
+    $scope.getPreviewForm = function () {
         if (!$scope.selectedFormId)  return "";
         var form = _.find($scope.forms, function (form) {
             return form.id == $scope.selectedFormId
         });
         return form.html;
-//        return '../../moduleResources/html5forms/html5forms/form-' + $scope.selectedFormId + '.html';
     };
 
     $scope.selectForm = function (id) {

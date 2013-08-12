7bf46d6c3601c2481943d3feaead8b88c4b9a1c9
Zabil <zabilcm@thoughtworks.com>
2013-08-12 14:04:13 +0200

    Removed unused classes which are not require for xform validation

--- javarosa/src/main/java/org/javarosa/core/api/ILogger.java
+++ javarosa/src/main/java/org/javarosa/core/api/ILogger.java
@@ -19,10 +19,6 @@
  */
 package org.javarosa.core.api;
 
-import org.javarosa.core.log.IFullLogSerializer;
-import org.javarosa.core.log.StreamLogSerializer;
-
-import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -36,17 +32,8 @@ import java.util.Date;
 public interface ILogger {
 	
 	public void log(String type, String message, Date logDate);
-	
-	public void clearLogs();
-	
-	public <T> T serializeLogs(IFullLogSerializer<T> serializer);
-	
-	public void serializeLogs(StreamLogSerializer serializer) throws IOException;
-	public void serializeLogs(StreamLogSerializer serializer, int limit) throws IOException;
-	
-	public void panic();
-	
-	public int logSize();
-	
-	public void halt();
+
+    public void panic();
+
+    public void halt();
 }

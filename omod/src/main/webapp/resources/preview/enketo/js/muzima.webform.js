a41fcdd53ad4e7dd5cd23564bf19f9832b52ab39
Petra <pkritzi@thoughtworks.com>
2013-12-18 11:17:41 +0200

    Synchronize css js html from muzima android

--- omod/src/main/webapp/resources/preview/enketo/js/muzima.webform.js
+++ omod/src/main/webapp/resources/preview/enketo/js/muzima.webform.js
@@ -39,9 +39,11 @@ $(document).ready(function () {
     loadErrors = form.init();
 
     function save(status) {
-        var jData = jDataO.get();
-        delete jData.errors;
-        formDataController.save(jData, status);
+        formDataRepositoryContext.showSaveProgressBar();
+        var jsonData = jDataO.get();
+        var xmlData = jDataO.toXML();
+        delete jsonData.errors;
+        formDataController.save(jsonData, xmlData, status);
     }
 
     document.saveDraft = function () {
@@ -82,12 +84,12 @@ $(document).ready(function () {
     $barcodeInput.before("<input type='button' class='barcode_img'>");
     $('.barcode_img').click(function () {
         //barCodeComponent is defined in FormWebViewActivity.java
-        if(!isFromInput){
+        if (!isFromInput) {
             barCodeComponent.startBarCodeIntent($barcodeInput.attr('name'));
         }
         isFromInput = false;
     });
-    $barcodeInput.click(function (e){
+    $barcodeInput.click(function (e) {
         isFromInput = true;
     });
     /*End- BarCode Functionality*/
@@ -141,5 +143,4 @@ $(document).ready(function () {
     });
 
     /* Multi select Hack - End */
-
 });
